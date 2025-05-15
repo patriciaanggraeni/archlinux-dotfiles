90c34a95903c0fc5ff0c776864601717fa4c2ce3
patriciaanggraeni <susiloangga17@gmail.com>
2025-05-15 15:22:53 +0700

    perbaikan symlink

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ ln -s ~/.config/archlinux-dotfiles/starship ~/.config/starship
 
 # ----- files
 ln -s ~/.config/archlinux-dotfiles/cava/config ~/.config/cava/config
-ln -s ~/.config/archlinux-dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/.config/archlinux-dotfiles/nvim ~/.config/nvim
 ln -s ~/.config/archlinux-dotfiles/btop/btop.conf ~/.config/btop/btop.conf
 ln -s ~/.config/archlinux-dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
 ln -s ~/.config/archlinux-dotfiles/waybar/config.json ~/.config/waybar/config.json
-ln -s ~/.config/archlinux-dotfiles/fastfetch/config.conf ~/.config/fastfetch/config.conf
+ln -s ~/.config/archlinux-dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
 # ln -s ~/.config/archlinux-dotfiles/cava/config ~/.config/cava/config
 # ln -s ~/.config/archlinux-dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
