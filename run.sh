e2606fb6d49755dab9da9a34e8babc46c069e597
patriciaanggraeni <susiloangga17@gmail.com>
2025-05-27 17:12:50 +0700

    perbaikan kecil pada path

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 # ----- directory
+ln -s ~/.config/archlinux-dotfiles/hypr ~/.config/hypr
 ln -s ~/.config/archlinux-dotfiles/kitty ~/.config/kitty
 ln -s ~/.config/archlinux-dotfiles/starship ~/.config/starship
 
 # ----- files
-ln -s ~/.config/archlinux-dotfiles/cava/config ~/.config/cava/config
 ln -s ~/.config/archlinux-dotfiles/nvim ~/.config/nvim
+ln -s ~/.config/archlinux-dotfiles/waybar ~/.config/waybar
+ln -s ~/.config/archlinux-dotfiles/cava/config ~/.config/cava/config
 ln -s ~/.config/archlinux-dotfiles/btop/btop.conf ~/.config/btop/btop.conf
 ln -s ~/.config/archlinux-dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -s ~/.config/archlinux-dotfiles/waybar/config.json ~/.config/waybar/config.json
 ln -s ~/.config/archlinux-dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
-# ln -s ~/.config/archlinux-dotfiles/cava/config ~/.config/cava/config
-# ln -s ~/.config/archlinux-dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
-# ln -s ~/.config/archlinux-dotfiles/starship/statship.toml ~/.config/starship/statship.toml
+# ----- themes
+ln -s ~/.config/archlinux-dotfiles/index.theme ~/.icons/default
+
+# ----- run execution permision for script
+chmod +x ~/.config/archlinux-dotfiles/hypr/scripts/autostart/*.sh
